bf5fd9b074d8e56ee06bcd78394e3fe4c1f33e3c
vladimir <vladimir.obradovic.sa@hotmail.com>
2021-09-15 13:54:39 +0200

    dodao index.js

---
+++ ex01/src/index.js
@@ -0,0 +1,7 @@
+function main() {
+    // Only change code below this line
+    console.log("Hello World!");
+    // Only change code above this line
+    }
+    console.log(main());
+    module.exports = main;
