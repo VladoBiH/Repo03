4005300905c4ecd96002143096f4ddd20ff62df9
vladimir <vladimir.obradovic.sa@hotmail.com>
2021-09-15 14:30:26 +0200

    dodao index.js file

---
+++ ex02/src/index.js
@@ -0,0 +1,8 @@
+function main() {
+    // Only change code below this line
+    // comment 1
+    /* comment 2*/
+    // Only change code above this line
+    }
+    console.log(main());
+    module.exports = main;
