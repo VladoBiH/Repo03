21f866b87b31c98c2a8fc9337e231a57cb65f6b1
vladimir <vladimir.obradovic.sa@hotmail.com>
2021-09-16 11:37:28 +0200

    ispravljanje varijable

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -1,7 +1,5 @@
 function main(numOne, numTwo) {
     // Only change code below this line
-    var numOne;
-    var numTwo;
     var difference;
     difference = numOne - numTwo;
     // Only change code above this line
